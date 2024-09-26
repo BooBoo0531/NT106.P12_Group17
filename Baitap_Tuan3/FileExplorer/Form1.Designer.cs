8a5ee3cd5b7bb4db3de748facc62faf8f4cb6b00
BooBoo0531 <174261082+BooBoo0531@users.noreply.github.com>
2024-09-26 22:00:27 +0700

    Add files via upload

---
+++ Baitap_Tuan3/FileExplorer/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace FileExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonNew = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonCopy = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonCut = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonPaste = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonDelete = new System.Windows.Forms.ToolStripButton();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView = new System.Windows.Forms.ListView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonNew,
+            this.toolStripButtonCopy,
+            this.toolStripButtonCut,
+            this.toolStripButtonPaste,
+            this.toolStripButtonDelete});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonNew
+            // 
+            this.toolStripButtonNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonNew.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonNew.Image")));
+            this.toolStripButtonNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonNew.Name = "toolStripButtonNew";
+            this.toolStripButtonNew.Size = new System.Drawing.Size(87, 24);
+            this.toolStripButtonNew.Text = "New folder";
+            this.toolStripButtonNew.Click += new System.EventHandler(this.toolStripButtonNew_Click);
+            // 
+            // toolStripButtonCopy
+            // 
+            this.toolStripButtonCopy.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonCopy.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonCopy.Image")));
+            this.toolStripButtonCopy.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonCopy.Name = "toolStripButtonCopy";
+            this.toolStripButtonCopy.Size = new System.Drawing.Size(47, 24);
+            this.toolStripButtonCopy.Text = "Copy";
+            this.toolStripButtonCopy.Click += new System.EventHandler(this.toolStripButtonCopy_Click);
+            // 
+            // toolStripButtonCut
+            // 
+            this.toolStripButtonCut.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonCut.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonCut.Image")));
+            this.toolStripButtonCut.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonCut.Name = "toolStripButtonCut";
+            this.toolStripButtonCut.Size = new System.Drawing.Size(35, 24);
+            this.toolStripButtonCut.Text = "Cut";
+            this.toolStripButtonCut.Click += new System.EventHandler(this.toolStripButtonCut_Click);
+            // 
+            // toolStripButtonPaste
+            // 
+            this.toolStripButtonPaste.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonPaste.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonPaste.Image")));
+            this.toolStripButtonPaste.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonPaste.Name = "toolStripButtonPaste";
+            this.toolStripButtonPaste.Size = new System.Drawing.Size(47, 24);
+            this.toolStripButtonPaste.Text = "Paste";
+            this.toolStripButtonPaste.Click += new System.EventHandler(this.toolStripButtonPaste_Click);
+            // 
+            // toolStripButtonDelete
+            // 
+            this.toolStripButtonDelete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonDelete.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonDelete.Image")));
+            this.toolStripButtonDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonDelete.Name = "toolStripButtonDelete";
+            this.toolStripButtonDelete.Size = new System.Drawing.Size(57, 24);
+            this.toolStripButtonDelete.Text = "Delete";
+            this.toolStripButtonDelete.Click += new System.EventHandler(this.toolStripButtonDelete_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 27);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 423);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // treeView
+            // 
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(266, 423);
+            this.treeView.TabIndex = 0;
+            this.treeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView_BeforeExpand);
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Type";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Modified";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 150;
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(0, 0);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(530, 423);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonNew;
+        private System.Windows.Forms.ToolStripButton toolStripButtonCopy;
+        private System.Windows.Forms.ToolStripButton toolStripButtonCut;
+        private System.Windows.Forms.ToolStripButton toolStripButtonPaste;
+        private System.Windows.Forms.ToolStripButton toolStripButtonDelete;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ Baitap_Tuan3/FileExplorer/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void ShowFileNames()
+        {
+            DirectoryInfo di = new DirectoryInfo(treeView.SelectedNode.FullPath);
+            FileInfo[] fiarray = { };
+            ListViewItem item;
+
+            listView.Items.Clear();
+
+            if (di.Exists)
+            {
+                fiarray = di.GetFiles();
+            }
+
+            listView.BeginUpdate();
+            foreach (FileInfo fi in fiarray)
+            {
+                item = new ListViewItem(fi.Name);
+                item.SubItems.Add(fi.Extension);
+                item.SubItems.Add(fi.LastWriteTime.ToString());
+                listView.Items.Add(item);
+            }
+            listView.EndUpdate();
+        }
+
+        public void ShowDrives()
+        {
+            treeView.BeginUpdate();
+            string[] drives = Directory.GetLogicalDrives();
+            foreach (string drive in drives)
+            {
+                TreeNode tn = new TreeNode(drive);
+                treeView.Nodes.Add(tn);
+                AddDirs(tn);
+            }
+            treeView.EndUpdate();
+        }
+
+        public void AddDirs(TreeNode tn)
+        {
+            string path = tn.FullPath;
+            DirectoryInfo di = new DirectoryInfo(path);
+            DirectoryInfo[] diarray = { };
+
+            try
+            {
+                if (di.Exists)
+                {
+                    diarray = di.GetDirectories();
+                }
+            }
+            catch
+            {
+                return;
+            }
+
+            foreach (DirectoryInfo d in diarray)
+            {
+                TreeNode tndir = new TreeNode(d.Name);
+                tn.Nodes.Add(tndir);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowDrives();
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            ShowFileNames();
+        }
+
+        private void treeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            treeView.BeginUpdate();
+            foreach (TreeNode tn in e.Node.Nodes)
+            {
+                AddDirs(tn);
+            }
+            treeView.EndUpdate();
+        }
+
+        
+
+        // Ham cac chuc nang
+        private void CreateNewFolder()
+        {
+            if (treeView.SelectedNode != null)
+            {
+                string folderPath = treeView.SelectedNode.FullPath;
+                string newFolderPath = Path.Combine(folderPath, "New Folder");
+
+                try
+                {
+                    Directory.CreateDirectory(newFolderPath);
+                    TreeNode newNode = new TreeNode("New Folder");
+                    treeView.SelectedNode.Nodes.Add(newNode);
+                    MessageBox.Show("New folder created!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error creating folder: " + ex.Message);
+                }
+            }
+        }
+
+        private string fileToCopyOrCut;
+        private bool isCutOperation;
+
+        private void CopyFile()
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                fileToCopyOrCut = Path.Combine(treeView.SelectedNode.FullPath, listView.SelectedItems[0].Text);
+                isCutOperation = false;
+                MessageBox.Show("File copied: " + fileToCopyOrCut);
+            }
+        }
+
+        private void CutFile()
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                fileToCopyOrCut = Path.Combine(treeView.SelectedNode.FullPath, listView.SelectedItems[0].Text);
+                isCutOperation = true;
+                MessageBox.Show("File cut: " + fileToCopyOrCut);
+            }
+        }
+
+        private void PasteFile()
+        {
+            if (!string.IsNullOrEmpty(fileToCopyOrCut) && treeView.SelectedNode != null)
+            {
+                string destinationPath = Path.Combine(treeView.SelectedNode.FullPath, Path.GetFileName(fileToCopyOrCut));
+
+                try
+                {
+                    if (isCutOperation)
+                    {
+                        File.Move(fileToCopyOrCut, destinationPath);
+                        MessageBox.Show("File moved to: " + destinationPath);
+                    }
+                    else
+                    {
+                        File.Copy(fileToCopyOrCut, destinationPath);
+                        MessageBox.Show("File copied to: " + destinationPath);
+                    }
+
+                    fileToCopyOrCut = null;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error during paste operation: " + ex.Message);
+                }
+            }
+        }
+
+        private void DeleteFile()
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                string filePath = Path.Combine(treeView.SelectedNode.FullPath, listView.SelectedItems[0].Text);
+
+                try
+                {
+                    if (File.Exists(filePath))
+                    {
+                        File.Delete(filePath);
+                        MessageBox.Show("File deleted: " + filePath);
+                    }
+                    else if (Directory.Exists(filePath))
+                    {
+                        Directory.Delete(filePath, true); 
+                        MessageBox.Show("Folder deleted: " + filePath);
+                    }
+                    ShowFileNames(); 
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error deleting file: " + ex.Message);
+                }
+            }
+        }
+
+        private void toolStripButtonNew_Click(object sender, EventArgs e)
+        {
+            CreateNewFolder();
+        }
+
+        private void toolStripButtonCopy_Click(object sender, EventArgs e)
+        {
+            CopyFile();
+        }
+
+        private void toolStripButtonCut_Click(object sender, EventArgs e)
+        {
+            CutFile();
+        }
+
+        private void toolStripButtonPaste_Click(object sender, EventArgs e)
+        {
+            PasteFile();
+        }
+
+        private void toolStripButtonDelete_Click(object sender, EventArgs e)
+        {
+            DeleteFile();  
+        }
+    }
+}
