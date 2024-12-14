581d92be6eb89aeb6741b25c75caa10fdc28fb64
BooBoo0531 <174261082+BooBoo0531@users.noreply.github.com>
2024-12-14 13:58:38 +0700

    Add files via upload

---
+++ Đồ án - Snake Multiplayers/Server/Snake_Royale_Server/Handlers/DTO/SnakeDTO.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Sockets;
+
+public class SnakeDTO
+{
+
+    public enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+
+    public enum State
+    {
+        Alive,
+        Dead
+    }
+    public string Username { get; set; }
+    public State CurrentState { get; set; }
+    public Direction CurrentDirection { get; set; }
+    public float PositionX { get; set; } = 0;
+    public float PositionY { get; set; } = 0;
+    public int SnakeBodySize { get; set; } = 0;
+
+    public long Score { get; set; } = 0;
+
+    public SnakeDTO()
+    {
+        CurrentDirection = Direction.Right;
+        CurrentState = State.Alive;
+    }
+}
