173251939b4ec2c47d8bb34583f176824b242bed
BooBoo0531 <174261082+BooBoo0531@users.noreply.github.com>
2024-12-14 14:01:55 +0700

    Add files via upload

---
+++ Đồ án - Snake Multiplayers/Server/Snake_Royale_Server/Models/SnakeRoyaleDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+
+namespace Snake_Royale_Server.Models
+{
+    public class SnakeRoyaleDbContext : DbContext
+    {
+        public SnakeRoyaleDbContext()
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=SnakeRoyaleDB.db;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
